7736648c6e2126b667f7e5ebaca1c8190508f789
Freddie <fv4.programmer@gmail.com>
2015-04-15 10:22:23 -0400

    Create function for getting PDFs

--- VScraper.py
+++ VScraper.py
@@ -12,3 +12,15 @@ link_list = []
 file_names = []
 suffix = input("What type of file do you want to scrape? \
                \nExamples: .png, .pdf, .doc - ")
+response = requests.get(url, stream=True)
+soup = bs(response.text)
+
+def getPDFs():
+        
+    for link in soup.find_all('a'): # Finds all links
+        if suffix in str(link): # If the link ends in .pdf
+            link_list.append(link.get('href'))
+
+    for i in range(len(link_list)):
+        file_names.append(str(link_list[i]).replace('/[^/]*$',''))
+    print(file_names)
