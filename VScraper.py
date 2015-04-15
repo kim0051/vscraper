a91afe938d7a9a9a2bb7d7f5f41c9b54e57c14cc
Freddie <fv4.programmer@gmail.com>
2015-04-15 10:21:05 -0400

    Set up variables

--- VScraper.py
+++ VScraper.py
@@ -1,3 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
+
+
+url = input("Enter the URL you want to scrape from: ")
+print("")
+
+bs = BeautifulSoup
+
+link_list = []
+file_names = []
+suffix = input("What type of file do you want to scrape? \
+               \nExamples: .png, .pdf, .doc - ")
