9b1ab353fba33e10c37f070b5824d5a613f86639
Freddie <fv4.programmer@gmail.com>
2015-04-29 13:05:10 -0400

    Fix link concatenation

--- VScraper.py
+++ VScraper.py
@@ -28,7 +28,8 @@ def getFiles():
 
     i = 0
     for link in link_list:
-        urlretrieve(url.rsplit('/',1)[0] + link, filepath + '\\' + file_names[i])
+        print("Full stuff...", url.rsplit('/',1)[0] + link, filepath + '\\' + file_names[i])
+        urlretrieve(url.rsplit('/',1)[0] + '/' + link, filepath + '\\' + file_names[i])
         i += 1
             
     printMessage(link_list, suffix)
